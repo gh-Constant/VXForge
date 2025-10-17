6bf4d6c7a87d4ad5df57b9e3b5cc6a26f670cfec
Constant <constantsuchet@gmail.com>
2025-10-17 10:54:12 +0200

    Fix .gitignore: add .idea/ directory and remove tracked build files

--- cmake-build-debug/CMakeFiles/Engine.dir/Game/src/main.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Engine.dir/Game/src/main.cpp.o: \
-  /Users/constantsuchet/CLionProjects/VXForge/Game/src/main.cpp \
-  /Users/constantsuchet/CLionProjects/VXForge/Engine/include/Engine.h
